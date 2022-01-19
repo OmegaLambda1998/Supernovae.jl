0d226db11a1a8a499a1d9f61ba19fb8f6799cdb3
Patrick Armstrong <u6096820@anu.edu.au>
2022-01-19 15:57:19 +1100

    Refactorising

--- src/Plotting.jl
+++ src/Plotting.jl
@@ -1,5 +1,18 @@
+module Plotting
+
+# External Packages
+using CairoMakie
+CairoMakie.activate!(type = "svg")
+using Unitful, UnitfulAstro
+
+# Internal Packages
+using ..Data
+
+# Exports
+export plot_lightcurve, plot_lightcurve!
+
 # Plotting functions
-function plot!(fig, ax, supernova::Supernova, plot_config::Dict)
+function plot_lightcurve!(fig, ax, supernova::Supernova, plot_config::Dict)
     instruments = String[]
     filters = String[]
     time = Dict()
@@ -11,7 +24,7 @@ function plot!(fig, ax, supernova::Supernova, plot_config::Dict)
     time_unit = uparse(get(units, "time", "d"))
     flux_unit = uparse(get(units, "flux", "μJy"))
     names = get(plot_config, "names", nothing)
-    for obs in supernova.lightcurve
+    for obs in supernova.lightcurve.observations
         if !isnothing(names)
             if !(obs.name in names)
                 continue
@@ -23,7 +36,7 @@ function plot!(fig, ax, supernova::Supernova, plot_config::Dict)
             push!(legend_names, obs.name)
         end
         if !(obs.filter.name in filters)
-            elem = MarkerElement(color = plot_config["color"][obs.filter.name], marker = :circle)
+            elem = MarkerElement(color = plot_config["colour"][obs.filter.name], marker = :circle)
             push!(legend_plots, elem)
             push!(legend_names, obs.filter.name)
         end
@@ -40,26 +53,25 @@ function plot!(fig, ax, supernova::Supernova, plot_config::Dict)
     for (i, time_key) in enumerate(collect(keys(time)))
         flux_key = collect(keys(flux))[i]
         flux_err_key = collect(keys(flux_err))[i]
-        scatter!(ax, time[time_key], flux[flux_key], color = plot_config["color"][time_key[2]], marker = Meta.parse(plot_config["marker"][time_key[1]]))
-        errorbars!(ax, time[time_key], flux[flux_key], flux_err[flux_err_key], color = plot_config["color"][time_key[2]], marker = Meta.parse(plot_config["marker"][time_key[1]]))
+        scatter!(ax, time[time_key], flux[flux_key], color = plot_config["colour"][time_key[2]], marker = Meta.parse(plot_config["marker"][time_key[1]]))
+        errorbars!(ax, time[time_key], flux[flux_key], flux_err[flux_err_key], color = plot_config["colour"][time_key[2]], marker = Meta.parse(plot_config["marker"][time_key[1]]))
     end
     Legend(fig[1, 2], legend_plots, legend_names)
     return fig, ax
 end
 
-function plot(supernova::Supernova, plot_config::Dict)
+function plot_lightcurve(supernova::Supernova, plot_config::Dict)
     fig = Figure()
     units = get(plot_config, "unit", Dict())
     time_unit = uparse(get(units, "time", "d"))
     flux_unit = uparse(get(units, "flux", "μJy"))
     ax = Axis(fig[1, 1], xlabel = "Time [$time_unit]", ylabel = "Flux [$flux_unit]", title = supernova.name)
-    fig, ax = plot!(fig, ax, supernova, plot_config)
+    fig, ax = plot_lightcurve!(fig, ax, supernova, plot_config)
+    path = get(plot_config, "path", nothing)
+    if !isnothing(path)
+        save(path, fig)
+    end
     return fig, ax
 end
 
-function plot(supernova::Supernova, plot_config::Dict, path::AbstractString)
-    fig, ax = plot(supernova, plot_config)
-    save(path, fig)
 end
-
-
