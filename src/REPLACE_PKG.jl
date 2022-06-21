8167e525edd49bd2ab5bb0ac0ad9d95ea6cf21a5
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2022-06-21 11:40:39 +1000

    Change how script is called

--- src/REPLACE_PKG.jl
+++ src/REPLACE_PKG.jl
@@ -3,24 +3,53 @@ module REPLACE_PKG
 # External packages
 using TOML
 using OLUtils
+using ArgParse
 
 # Internal Packages
+include("RunModule.jl")
+using .RunModule: run_REPLACE_PKG
 
 # Exports
-export run_REPLACE_PKG
+export main 
 
-function run_REPLACE_PKG(toml::Dict, verbose::Bool)
-    setup_global!(toml, verbose)
-    config = toml["global"]
+Base.@ccallable function julia_main()::Cint
+    try
+        main()
+    catch
+        Base.invokelatest(Base.display_error, Base.catch_stack())
+        return 1
+    end
+    return 0
 end
 
-function run_REPLACE_PKG(toml_path::AbstractString, verbose::Bool)
-    toml = TOML.parsefile(toml_path)
+function get_args()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "--verbose", "-v"
+            help = "Increase level of logging verbosity"
+            action = :store_true
+        "input"
+            help = "Path to .toml file"
+            required = true
+    end
+    return parse_args(s)
+end
+
+function main()
+    args = get_args()
+    verbose = args["verbose"]
+    toml_path = args["input"]
+    toml = TOML.parsefil(abspath(toml_path))
     if !("global" in keys(toml))
         toml["global"] = Dict()
     end
     toml["global"]["toml_path"] = dirname(abspath(toml_path))
-    return run_REPLACE_PKG(toml, verbose)
+    setup_global!(toml, verbose)
+    run_REPLACE_PKG(toml)
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
 end
 
 end
---
+++ src/RunModule.jl
@@ -0,0 +1,13 @@
+module RunModule
+
+# External Packages
+
+# Internal Packages
+
+# Exports
+export run_REPLACE_PKG
+
+function run_REPLACE_PKG(toml::Dict)
+end
+
+end
