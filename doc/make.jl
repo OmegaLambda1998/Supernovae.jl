b2beea33dccd328e4f82b182751414e7086059bd
Patrick Armstrong <patrick.armstrong@anu.edu.au>
2024-07-04 19:03:40 +1000

    Fix doc build

--- doc/make.jl
+++ doc/make.jl
@@ -2,6 +2,7 @@ using Documenter
 using Pkg
 push!(LOAD_PATH, joinpath(@__DIR__, "../src/"))
 Pkg.develop(path=abspath(joinpath(@__DIR__, "../")))
+Pkg.build("Supernovae")
 using Supernovae
 
 DocMeta.setdocmeta!(Supernovae, :DocTestSetup, :(using Supernovae); recursive=true)
